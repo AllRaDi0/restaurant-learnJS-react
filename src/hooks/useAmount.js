6197e69836eb918b02ae52b8905b803af4fd1c6b
allradio <allradio@gmail.com>
2024-05-20 17:35:38 +0300

    add useAmount

---
+++ src/hooks/useAmount.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+export const useAmount = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count > 0 ? count - 1 : 0);
+  };
+
+  return { count, increment, decrement };
+};
