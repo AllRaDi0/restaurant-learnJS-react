8b0097ac772c987cd17874aedba15389e2ea4d55
allradio <allradio@gmail.com>
2024-05-20 17:39:22 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import Restaurants from "./components/Restaurants/Restaurants";
+import { restaurants } from "./fixtures";
+
+const App = () => {
+  return (
+    <div>
+      <Restaurants restaurants={restaurants} />
+    </div>
+  );
+};
 
 export default App;
